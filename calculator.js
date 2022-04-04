2e14eec99ce7331d44c004be723b8abbc9c4da1d
Eliott <fbgus333@naver.com>
2022-04-04 21:33:00 +0900

    Today I learnd

--- calculator.js
+++ calculator.js
@@ -1,23 +1,26 @@
 let str = ''
+// let putin = document.querySelector('.screen1').innerHTML;
 
 for (let i = 0; i < 9; i++) {
-document.querySelectorAll('.num')[i].addEventListener('click', function() {
-  document.querySelector('.screen1').innerHTML = i + 1;
-
-  str = str + (i+1)
-  document.querySelector('.screen1').innerHTML = str;
-
-});
+  document.querySelectorAll('.num')[i].addEventListener('click', function() {
+    document.querySelector('.screen1').innerHTML = i + 1;
+    
+    str = str + (i+1)
+    document.querySelector('.screen1').innerHTML = str;
+    
+  });
 }
+
 
 function logic() {
   let last_word = str.charAt(str.length-1);
   return parseInt(last_word);
 }
-let last_check = logic();
+
 
 
 document.querySelector('.clear').addEventListener('click', function (){
+
   str = '';
   document.querySelector('.screen1').innerHTML = str;
 
@@ -29,7 +32,7 @@ document.querySelector('.delete').addEventListener('click', function (){
 })
 
 document.querySelector('.dot').addEventListener('click', function (){
-  if (last_check != NaN){
+  if (isNaN(logic()) == false){
     str = str + '.';
     document.querySelector('.screen1').innerHTML = str;
   }
@@ -37,32 +40,38 @@ document.querySelector('.dot').addEventListener('click', function (){
 
 
 document.querySelector('.per').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '%';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
 document.querySelector('.divide').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '/';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
 document.querySelector('.multiple').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '*';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
 document.querySelector('.plus').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '+';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
 document.querySelector('.minus').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '-';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
 document.querySelector('.equal').addEventListener('click', function (){
+  if (isNaN(logic()) == false){
   str = str + '=';
   document.querySelector('.screen1').innerHTML = str;
-})
+}})
 
--- function.js
+++ function.js
@@ -1,10 +1,11 @@
-let str = 'eee2*3'
+// let str = 'eee2*3'
 
-function logic() {
-  let last_word = str.charAt(str.length-1);
-  return parseInt(last_word);
-}
+// function logic() {
+//   let last_word = str.charAt(str.length-1);
+//   return parseInt(last_word);
+// }
 
-console.log(logic());
+// console.log(logic());
 // console.log(last_word);
 // console.log(parseInt(last_word));
+
